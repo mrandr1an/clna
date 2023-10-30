676667ed42b02b8dc8fd6ee34e0a4b8f3833cb71
mrandr1an <krackedissad@gmail.com>
2023-10-30 13:15:26 +0000

    Added cleaner test implementation for now

--- test/linear/matrix_test.c
+++ test/linear/matrix_test.c
@@ -1,77 +1,31 @@
-
-/* //Enable debug functions */
+//Enable debug functions
 #define LNA_DEBUG
-/* //Features */
-#define LNA_FUNCTION_ERRORS
 
 //Include library
 #include "../../src/linear/algebra.h"
 
-#define matrixAdditionTest_FAIL
+static unsigned int test_counter = 0;
 
-int main() {
-  #ifdef fillMatrixTest
-  Matrix_ixj mat = initMatrix_ixj(2,4);
+#define GREEN "\x1B[32m"
+#define RED "\x1B[31m"
+#define RESET "\x1B[0m"
 
-  fillMatrix_pos_ixj(0,0,&mat,1.0);
-  fillMatrix_pos_ixj(0,1,&mat,2.0);
-  fillMatrix_pos_ixj(0,2,&mat,3.0);
-  fillMatrix_pos_ixj(0,3,&mat,4.0);
 
-  fillMatrix_pos_ixj(1,0,&mat,1.0);
-  fillMatrix_pos_ixj(1,1,&mat,2.0);
-  fillMatrix_pos_ixj(1,2,&mat,3.0);
-  fillMatrix_pos_ixj(1,3,&mat,4.0);
+// Test functions if result is unexpected call failed otherwise call passed
+// no reason to manually increase test_counter
+void passed()
+{
+printf(GREEN "####PASSED TEST:%d####\n" RESET, test_counter++);
+}
 
-  printMatrix_ixj(mat);
-  #endif // fillMatrixTest
+void failed()
+{
+printf(RED "####FAILED TEST:%d####\n" RESET, test_counter++);
+}
 
-  #ifdef matrixAdditionTest
-  Matrix_ixj mat1 = initMatrix_ixj(3,2);
-  Matrix_ixj mat2 = initMatrix_ixj(3,2);
+//Define test name
 
-  fillMatrix_pos_ixj(0,0,&mat1,1.0);
-  fillMatrix_pos_ixj(0,1,&mat1,3.0);
-  fillMatrix_pos_ixj(1,0,&mat1,1.0);
-  fillMatrix_pos_ixj(1,1,&mat1,0.0);
+int main()
+{
 
-  fillMatrix_pos_ixj(2,0,&mat1,1.0);
-  fillMatrix_pos_ixj(2,1,&mat1,2.0);
-
-  fillMatrix_pos_ixj(0,0,&mat2,0.0);
-  fillMatrix_pos_ixj(0,1,&mat2,0.0);
-
-  fillMatrix_pos_ixj(1,0,&mat2,7.0);
-  fillMatrix_pos_ixj(1,1,&mat2,5.0);
-
-  fillMatrix_pos_ixj(2,0,&mat2,2.0);
-  fillMatrix_pos_ixj(2,1,&mat2,1.0);
-
-  addMatrix_ixj_ixj(&mat1,mat2); 
-  printMatrix_ixj(mat1);
-  #endif //MatrixAdditiontest
-
-  #ifdef matrixAdditionTest_FAIL
-  Matrix_ixj mat1 = initMatrix_ixj(3,2);
-  Matrix_ixj mat2 = initMatrix_ixj(2,2);
-
-  fillMatrix_pos_ixj(0,0,&mat1,1.0);
-  fillMatrix_pos_ixj(0,1,&mat1,3.0);
-
-  fillMatrix_pos_ixj(1,0,&mat1,1.0);
-  fillMatrix_pos_ixj(1,1,&mat1,0.0);
-
-  fillMatrix_pos_ixj(2,0,&mat1,1.0);
-  fillMatrix_pos_ixj(2,1,&mat1,2.0);
-
-  fillMatrix_pos_ixj(0,0,&mat2,0.0);
-  fillMatrix_pos_ixj(0,1,&mat2,0.0);
-
-  fillMatrix_pos_ixj(1,0,&mat2,7.0);
-  fillMatrix_pos_ixj(1,1,&mat2,5.0);
-
-  addMatrix_ixj_ixj(&mat1,mat2); 
-  printMatrix_ixj(mat1);
-  #endif //matrixAdditionTest_Fail
-  return 0;
 }
